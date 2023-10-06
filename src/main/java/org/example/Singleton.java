839c38a6170014374773f21f0698e0c36db26d8e
beastthor <beastthor.ykc@gmail.com>
2023-10-06 07:06:01 -0400

    inner code commit

--- src/main/java/org/example/Singleton.java
+++ src/main/java/org/example/Singleton.java
@@ -1,2 +1,30 @@
-package org.example;public class Singleton {
+package org.example;
+
+public class Singleton {
+
+    //private static instance
+
+    private static Singleton instance;
+
+    //Private constructor to prevent external instantiation
+    private Singleton() {
+        //Initialization code if any
+    }
+
+    //public static method to get the instance
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+
+
+
+    //other methods and data members
+    public void doSomething() {
+        System.out.println("Singleton is doing soemthing");
+
+    }
 }
